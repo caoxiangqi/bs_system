e2cd3a39cd12bcc1448e315e1714a2b7a118ff10
cxq <c19977@163.com>
2016-06-27 17:48:15 +0800

    简历最新

---
+++ h5/js/index.js
@@ -0,0 +1,42 @@
+//解决移动端点击延迟300ms问题；
+FastClick.attach(document.body);
+
+// 适配HTML font-size
+(function () {
+    var winW = document.documentElement.clientWidth || document.body.clientWidth,
+        desW = 750;
+    document.documentElement.style.fontSize = winW / desW * 100 + "px";
+})();
+
+
+//创建个Swiper
+new Swiper(".swiper-container", {
+    loop: false,
+    direction: "vertical",
+    effect: "coverflow",
+    onSlidePrevEnd: changeEnd,
+    onSlideNextEnd: changeEnd
+});
+
+function changeEnd(swiper) {
+    var n = swiper.activeIndex,
+        slideAry = swiper.slides;
+    [].forEach.call(slideAry, function (slide, index) {
+        if (n === index) {
+            //slide.id = (n == 0) ? "page1" : "page2";
+            if (n == 0) {
+                slide.id = "page1"
+            } else if (n == 1) {
+                slide.id = "page2"
+            } else if (n == 2) {
+                slide.id = "page3"
+            } else if (n == 3) {
+                slide.id = "page4"
+            } else if (n == 4) {
+                slide.id = "page5"
+            }
+            return;
+        }
+        slide.id = "n" + index;
+    });
+}
