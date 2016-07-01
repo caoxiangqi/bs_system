cb278ae107ea081acc8457bd0b54ceb075a31833
cxq <c19977@163.com>
2016-07-01 23:26:40 +0800

    简历最新

--- 05.19/js/index.js
+++ 05.19/js/index.js
@@ -1,8 +1,10 @@
 //解决移动端点击延迟300ms问题；
 window.onload = function(){
-    var p1 = document.getElementsByClassName("page1")[0];
-    p1.id="page1";
-    console.log("t")
+    setTimeout(function(){
+        document.getElementById("mu0").remove();
+        var p1 = document.getElementsByClassName("page1")[0];
+        p1.id="page1";
+    },1500);
 };
 
 
@@ -48,3 +50,27 @@ function changeEnd(swiper) {
         slide.id = "n" + index;
     });
 }
+
+
+
+var music = document.getElementById("music"),
+    musicAudio = document.getElementById("musicAudio");
+window.setTimeout(function () {
+    musicAudio.play();//->让音频播放:浏览器开始下载资源文件,也就是让它播放到出声音还需要一段时间,只有发出声音后我们才会显示音乐的图标
+    musicAudio.addEventListener("canplay", function () {
+        //->canplay:音频文件已经可以播放了,但是不一定是所有资源都加载完成了,大部分是边播放边界
+        music.style.display = "block";
+        music.className = "music move";
+    }, false);
+}, 1000);
+music.addEventListener("click", function () {
+    //->当前是暂停状态我让其播放
+    if (musicAudio.paused) {
+        musicAudio.play();
+        music.className = "music move";
+        return;
+    }
+    //->当前是播放状态我让其暂停
+    musicAudio.pause();
+    music.className = "music";
+}, false);
